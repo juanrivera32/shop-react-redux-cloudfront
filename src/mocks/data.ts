48dda5b2bfd9a922f031090af052815988050708
Juan Rivera <guillerivera32@gmail.com>
2023-03-07 00:14:25 -0500

    update code

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -1,56 +1,50 @@
-import { OrderStatus } from "~/constants/order";
-import { CartItem } from "~/models/CartItem";
-import { Order } from "~/models/Order";
-import { AvailableProduct, Product } from "~/models/Product";
+import { OrderStatus } from '~/constants/order';
+import { CartItem } from '~/models/CartItem';
+import { Order } from '~/models/Order';
+import { AvailableProduct, Product } from '~/models/Product';
 
 export const products: Product[] = [
   {
-    description: "Short Product Description1",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    description: 'Short Product Description1',
+    id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
     price: 24,
-    title: "ProductOne",
-    category: 'Category 1',
-      author: 'Author 1',
+    title: 'ProductOne',
+    count: 3,
   },
   {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+    description: 'Short Product Description7',
+    id: '7567ec4b-b10c-48c5-9345-fc73c48a80a1',
     price: 15,
-    title: "ProductTitle",
-    category: 'Category 2',
-      author: 'Author 2',
+    title: 'ProductTitle',
+    count: 3,
   },
   {
-    description: "Short Product Description2",
-    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+    description: 'Short Product Description2',
+    id: '7567ec4b-b10c-48c5-9345-fc73c48a80a3',
     price: 23,
-    title: "Product",
-    category: 'Category 3',
-      author: 'Author 3',
+    title: 'Product',
+    count: 3,
   },
   {
-    description: "Short Product Description4",
-    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+    description: 'Short Product Description4',
+    id: '7567ec4b-b10c-48c5-9345-fc73348a80a1',
     price: 15,
-    title: "ProductTest",
-    category: 'Category 4',
-    author: 'Author 4',
+    title: 'ProductTest',
+    count: 3,
   },
   {
-    description: "Short Product Descriptio1",
-    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+    description: 'Short Product Descriptio1',
+    id: '7567ec4b-b10c-48c5-9445-fc73c48a80a2',
     price: 23,
-    title: "Product2",
-    category: 'Category 1',
-    author: 'Author 4',
+    title: 'Product2',
+    count: 3,
   },
   {
-    description: "Short Product Description7",
-    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+    description: 'Short Product Description7',
+    id: '7567ec4b-b10c-45c5-9345-fc73c48a80a1',
     price: 15,
-    title: "ProductName",
-    category: 'Category 3',
-      author: 'Author 5',
+    title: 'ProductName',
+    count: 3,
   },
 ];
 
@@ -61,23 +55,23 @@ export const availableProducts: AvailableProduct[] = products.map(
 export const cart: CartItem[] = [
   {
     product: {
-      description: "Short Product Description1",
-      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-      category: 'Category 1',
-      author: 'Author 1',
+      description: 'Short Product Description1',
+      id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
+
+      count: 3,
       price: 24,
-      title: "ProductOne",
+      title: 'ProductOne',
     },
     count: 2,
   },
   {
     product: {
-      description: "Short Product Description7",
-      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      description: 'Short Product Description7',
+      id: '7567ec4b-b10c-45c5-9345-fc73c48a80a1',
       price: 15,
-      category: 'Category 2',
-      author: 'Author 2',
-      title: "ProductName",
+
+      count: 3,
+      title: 'ProductName',
     },
     count: 5,
   },
@@ -85,35 +79,35 @@ export const cart: CartItem[] = [
 
 export const orders: Order[] = [
   {
-    id: "1",
+    id: '1',
     address: {
-      address: "some address",
-      firstName: "Name",
-      lastName: "Surname",
-      comment: "",
+      address: 'some address',
+      firstName: 'Name',
+      lastName: 'Surname',
+      comment: '',
     },
     items: [
-      { productId: "7567ec4b-b10c-48c5-9345-fc73c48a80aa", count: 2 },
-      { productId: "7567ec4b-b10c-45c5-9345-fc73c48a80a1", count: 5 },
+      { productId: '7567ec4b-b10c-48c5-9345-fc73c48a80aa', count: 2 },
+      { productId: '7567ec4b-b10c-45c5-9345-fc73c48a80a1', count: 5 },
     ],
     statusHistory: [
-      { status: OrderStatus.Open, timestamp: Date.now(), comment: "New order" },
+      { status: OrderStatus.Open, timestamp: Date.now(), comment: 'New order' },
     ],
   },
   {
-    id: "2",
+    id: '2',
     address: {
-      address: "another address",
-      firstName: "John",
-      lastName: "Doe",
-      comment: "Ship fast!",
+      address: 'another address',
+      firstName: 'John',
+      lastName: 'Doe',
+      comment: 'Ship fast!',
     },
-    items: [{ productId: "7567ec4b-b10c-48c5-9345-fc73c48a80aa", count: 3 }],
+    items: [{ productId: '7567ec4b-b10c-48c5-9345-fc73c48a80aa', count: 3 }],
     statusHistory: [
       {
         status: OrderStatus.Sent,
         timestamp: Date.now(),
-        comment: "Fancy order",
+        comment: 'Fancy order',
       },
     ],
   },
