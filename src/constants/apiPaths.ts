9aa2d8187ec5552c2e5191e496801c23d2d2d996
Juan Rivera <guillerivera32@gmail.com>
2023-04-05 00:47:32 -0500

    update products url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://u43tgdmc21.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://1hi0tvu64l.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://vnrzjgt9i3.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
